b805a96196c76bc0a643f2803dd6a8ea26b34909
MHDSAEEDRABID <saeedrubid@gmail.com>
2021-03-23 20:04:32 +0200

    fix bug2

---
+++ WindowsFormsApplication30/School.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Text;
+namespace Shabeba
+{
+    public partial class School : Form
+    {
+        public School()
+        {
+            InitializeComponent();
+        }
+
+        private void School_Load(object sender, EventArgs e)
+        {
+            PrivateFontCollection privateFont = new PrivateFontCollection();
+            privateFont.AddFontFile("font.ttf");
+        }
+    }
+}
