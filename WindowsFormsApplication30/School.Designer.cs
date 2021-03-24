334f912d72e347612a96cdf53157416b1de0cc8d
MHDSAEEDRABID <saeedrubid@gmail.com>
2021-03-24 11:55:09 +0200

    Build Add button functionalty

--- WindowsFormsApplication30/School.Designer.cs
+++ WindowsFormsApplication30/School.Designer.cs
@@ -30,26 +30,246 @@ namespace Shabeba
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNameManager = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtShcoolPhone = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNumberOfManager = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(658, 30);
+            this.label1.Location = new System.Drawing.Point(997, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(108, 25);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "رقم المدرسة";
+            // 
+            // txtId
+            // 
+            this.txtId.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtId.Location = new System.Drawing.Point(805, 32);
+            this.txtId.Margin = new System.Windows.Forms.Padding(6);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(180, 33);
+            this.txtId.TabIndex = 1;
+            this.txtId.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtId_KeyPress);
+            // 
+            // txtName
+            // 
+            this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtName.Location = new System.Drawing.Point(805, 106);
+            this.txtName.Margin = new System.Windows.Forms.Padding(6);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(180, 33);
+            this.txtName.TabIndex = 3;
+            this.txtName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtName_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(997, 109);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "اسم المدرسة";
+            // 
+            // txtNameManager
+            // 
+            this.txtNameManager.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtNameManager.Location = new System.Drawing.Point(458, 106);
+            this.txtNameManager.Margin = new System.Windows.Forms.Padding(6);
+            this.txtNameManager.Name = "txtNameManager";
+            this.txtNameManager.Size = new System.Drawing.Size(180, 33);
+            this.txtNameManager.TabIndex = 7;
+            this.txtNameManager.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtName_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(652, 111);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "اسم المدير";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtAddress.Location = new System.Drawing.Point(458, 32);
+            this.txtAddress.Margin = new System.Windows.Forms.Padding(6);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(180, 33);
+            this.txtAddress.TabIndex = 5;
+            this.txtAddress.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtName_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(652, 37);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "العنوان";
+            // 
+            // txtShcoolPhone
+            // 
+            this.txtShcoolPhone.Location = new System.Drawing.Point(63, 104);
+            this.txtShcoolPhone.Margin = new System.Windows.Forms.Padding(6);
+            this.txtShcoolPhone.Name = "txtShcoolPhone";
+            this.txtShcoolPhone.Size = new System.Drawing.Size(180, 33);
+            this.txtShcoolPhone.TabIndex = 11;
+            this.txtShcoolPhone.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtId_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(257, 109);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "هاتف المدرسة";
+            // 
+            // txtNumberOfManager
+            // 
+            this.txtNumberOfManager.Location = new System.Drawing.Point(63, 30);
+            this.txtNumberOfManager.Margin = new System.Windows.Forms.Padding(6);
+            this.txtNumberOfManager.Name = "txtNumberOfManager";
+            this.txtNumberOfManager.Size = new System.Drawing.Size(180, 33);
+            this.txtNumberOfManager.TabIndex = 9;
+            this.txtNumberOfManager.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtId_KeyPress);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(257, 35);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 25);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "هاتف المدير";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(927, 201);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(128, 45);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "إضافة";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(652, 201);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(128, 45);
+            this.btnEdit.TabIndex = 13;
+            this.btnEdit.Text = "تعديل";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(342, 201);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(128, 45);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "حذف";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(63, 201);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(128, 45);
+            this.btnReset.TabIndex = 15;
+            this.btnReset.Text = "مسح";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgv.Location = new System.Drawing.Point(0, 318);
+            this.dgv.Name = "dgv";
+            this.dgv.RowTemplate.Height = 40;
+            this.dgv.Size = new System.Drawing.Size(1152, 302);
+            this.dgv.TabIndex = 16;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(393, 272);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(325, 33);
+            this.textBox1.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(758, 275);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(113, 25);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "اسم المدرسة";
             // 
             // School
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1152, 620);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtShcoolPhone);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtNumberOfManager);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtNameManager);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtId);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "School";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.Text = "School";
             this.Load += new System.EventHandler(this.School_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,5 +278,23 @@ namespace Shabeba
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNameManager;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtShcoolPhone;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNumberOfManager;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
     }
 }
